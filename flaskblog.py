f8bb9f79c594c23ce547327fe6d3ec4264a716cf
Bartek <bartek578@gmail.com>
2022-03-09 21:26:21 +0100

    some changes

--- flaskblog.py
+++ flaskblog.py
@@ -1,20 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "9cac665aaa015830e2eecc9977977f68"
 
 # set FLASK_APP=flaskblog.py
 # flask run
 # set FLASK_DEBUG=1
 
+posts = [
+    {
+        "author": "Bartłomiej Bielawa",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2018"
+    },
+    {
+        "author": "Adam Małysz",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018"
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
